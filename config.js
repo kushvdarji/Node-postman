2d9a85510490b98e2e29ce726592327fdfbe6eac
kushd <kush.darji@xceltec.in>
2023-02-17 10:05:49 +0530

    modified

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
-// const knex=require("knex");
-
-//     let con=knex({ client:"mysql",
-//     connection:{
-//         host:"127.0.0.1",
-//         user:"root",
-//         password:"",
-//         database:"knex"
-//     }})
-   
-// module.export=con;
-module.exports = {
-    client:"mysql",
+const knex=require("knex");
+    let con={ 
+        client:"mysql",
     connection:{
-                host:"127.0.0.1",
-                user:"root",
-                password:"",
-                database:"Knex"
-            }
-};
+        host:"127.0.0.1",
+        user:"root",
+        password:"",
+        database:"knex"
+    }}
+module.exports=con;
+// module.exports = {
+//     client:"mysql",
+//     connection:{
+//                 host:"127.0.0.1",
+//                 user:"root",
+//                 password:"",
+//                 database:"Knex"
+//             }
+// };
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var bodyParser=require("body-parser")
 const app =express();
 const knex = require('knex')(require('./config'))
-app.get("/", (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/get", (req, res) => {
   try{
   knex('Kush').select('*').then((data)=> { 
   console.log(data);
@@ -13,6 +16,25 @@ app.get("/", (req, res) => {
     console.log(err);
   }
 });
+app.post("/insert",(req,res)=>{
+try {
+    let data={
+      ID:req.body.ID,
+      Name:req.body.Name,
+      Age:req.body.Age
+    }
+    knex('Kush')
+    .insert(data).then(async(check)=>
+    {
+      console.log("inserted "+check);
+      res.send("inserted "+check)
+    },(err)=>{
+      if(err){
+        if(err.sqlMessage){
+          res.send(err.sqlMessage)
+        }
+  res.send(err)}})} catch (error) {
+  res.send(error)}})
 app.listen(3500, function(){
-  console.log("server");
-});
+  console.log("server");});
+  
